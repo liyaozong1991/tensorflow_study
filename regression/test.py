f269bb6137c78dc56268dff505d4ad9a20a604af
liyaozong <liyaozong1991@gmail.com>
2018-08-31 17:37:11 +0800

    add regression

---
+++ regression/test.py
@@ -0,0 +1,137 @@
+# @title MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+boston_housing = keras.datasets.boston_housing
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
+print("Training set: {}".format(train_data.shape))  # 404 examples, 13 features
+print("Testing set:  {}".format(test_data.shape))   # 102 examples, 13 features
+print(train_data[0])  # Display sample features, notice the different scales
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+                'TAX', 'PTRATIO', 'B', 'LSTAT']
+
+df = pd.DataFrame(train_data, columns=column_names)
+print(df.head())
+print(train_labels[0:10])  # Display first 10 entries
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+print(train_data[0])  # First training sample, normalized
+
+
+def build_model():
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation=tf.nn.relu,
+                           input_shape=(train_data.shape[1],)),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dense(1)
+    ])
+
+    optimizer = tf.train.RMSPropOptimizer(0.001)
+
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae'])
+    return model
+
+
+model = build_model()
+print(model.summary())
+
+
+# Display training progress by printing a single dot for each completed epoch
+class PrintDot(keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0:
+            print('')
+        print('.', end='')
+
+
+EPOCHS = 500
+
+# Store training stats
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0,
+                    callbacks=[PrintDot()])
+
+
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [1000$]')
+    plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+             label='Train Loss')
+    plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+             label = 'Val loss')
+    plt.legend()
+    plt.ylim([0, 5])
+    plt.show()
+
+
+plot_history(history)
+model = build_model()
+
+# The patience parameter is the amount of epochs to check for improvement
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0,
+                    callbacks=[early_stop, PrintDot()])
+
+plot_history(history)
+
+[loss, mae] = model.evaluate(test_data, test_labels, verbose=0)
+print("\nTesting set Mean Abs Error: ${:7.2f}".format(mae * 1000))
+
+test_predictions = model.predict(test_data).flatten()
+
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [1000$]')
+plt.ylabel('Predictions [1000$]')
+plt.axis('equal')
+plt.xlim(plt.xlim())
+plt.ylim(plt.ylim())
+_ = plt.plot([-100, 100], [-100, 100])
+plt.show()
+
+error = test_predictions - test_labels
+plt.hist(error, bins = 50)
+plt.xlabel("Prediction Error [1000$]")
+_ = plt.ylabel("Count")
+plt.show()
